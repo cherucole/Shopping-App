043312fa2de745631f063eb37691275968456706
cherucole <cheruyotcheruyot@gmail.com>
2019-12-05 13:24:42 +0300

    products showing basic

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,8 +1,20 @@
-import React, { Component } from 'react';
-import { View, Text, FlatList } from 'react-native';
+import React, { Component } from "react";
+import { View, Text, FlatList } from "react-native";
+import { useSelector } from "react-redux";
 
-const ProductsOverviewScreen = props =>{
-    return <FlatList />
-}
+const ProductsOverviewScreen = props => {
+  const products = useSelector(state => state.products.availableProducts);
+  return (
+    <FlatList
+      data={products}
+      keyExtractor={item => item.id}
+      renderItem={itemData => <Text>{itemData.item.title}</Text>}
+    />
+  );
+};
+
+ProductsOverviewScreen.navigationOptions = {
+  headerTitle: "All Products"
+};
 
 export default ProductsOverviewScreen;
