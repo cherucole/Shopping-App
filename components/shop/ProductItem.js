791d2be58c99f960370203d283302554b419fe06
cherucole <cheruyotcheruyot@gmail.com>
2019-12-09 19:23:40 +0300

    added validation through reducer

--- components/shop/ProductItem.js
+++ components/shop/ProductItem.js
@@ -9,7 +9,7 @@ import {
   Platform
 } from "react-native";
 
-import Card from '../UI/Card'
+import Card from "../UI/Card";
 
 const ProductItem = props => {
   let TouchableCmp = TouchableOpacity;
@@ -27,7 +27,7 @@ const ProductItem = props => {
             </View>
             <View style={styles.details}>
               <Text style={styles.title}>{props.title}</Text>
-              <Text style={styles.price}>${props.price.toFixed(2)}</Text>
+              <Text style={styles.price}>${props.price}</Text>
             </View>
             <View style={styles.actions}>{props.children}</View>
           </View>
